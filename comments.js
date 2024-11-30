0cb84a13b6c7f148980d20fe7899ef6e9cda730e
DostonbekZaripov <dastandev0110@gmail.com>
2024-11-30 13:49:42 +0500

    End Faqat Landing page qoldi

---
+++ comments.js
@@ -0,0 +1,63 @@
+// Swicht case ishlanishi;
+// let color = "blue";
+// switch (color) {
+//     case "blue":
+//         console.log("kok")
+//         break;
+//     case "red":
+//         console.log("qizil")
+//         break;
+//     case "black":
+//         console.log("qora")
+//         break;
+//     case "yellow":
+//         console.log("sariq")
+//         break;
+//     default:
+//         console.log("Error")
+//         break;
+// }
+
+// let str = "hello world"
+// let result = str + " ";
+// for(let i=0; i<result.length; i++){
+//     result[i]=result[i][0].toUpperCase()+str[i].slice(1)
+// }
+// result = result + (" ");
+// console.log(result)
+
+// let str = "Hello";
+// let str2;
+// for(let i = 0; i<str.length; i++){
+//     str2 += str.charCodeAt(i)
+// }
+// console.log(str2);
+
+// let matn = "Hello world";
+// let unlilar = "aouei";
+// let result = "";
+// for(let i = 0; i<matn.length; i++){
+//     if(unlilar.includes(matn[i])){
+//         result +=matn[i]
+//     }
+// }
+// console.log(result)
+
+// let matn = "WeBiN";
+// let katta = "";
+// for(let i = 0; i< matn.length; i++){
+//     if(matn[i] == matn[i].toUpperCase()){
+//         katta += matn[i]
+//     }
+// }
+// console.log(katta);
+
+// let str = '13468';
+// let sum = '';
+// for(let i = 0; i < str.length; i++){
+//     if(str.length[i] == +str.length[i] && 1 <= 9  ){
+//         sum += +str.length[i]
+//     }
+// }
+// console.log(sum)
+
---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+// 1-usuli
+// let matn = "Salom dunyo";
+// switch (matn) {
+//     case "Salom dunyo":
+//         console.log("dunyo Salom")
+//         break;
+//     default:
+//         console.log("Hato")
+//         break;
+// }
+
+// 2-usuli
+// let matn = "Salom dunyo";
+// let sozlar = matn.split(' ');
+// let teskari_matn = '';
+// for(let i = sozlar.length - 1; i >= 0; i-- ){
+//     teskari_matn += sozlar[i];
+//     if(i>0) teskari_matn += ' '
+// }
+// console.log(teskari_matn);
+
+// 3-masala
+// let matn = "Salom Dunyo Hello World";
+// let sozlar = matn.split(' ')
+// let bosh_harflar = 0
+
+// for(let i = 0; i<sozlar.length; i++){
+//     bosh_harflar += 1;
+// }
+// console.log(bosh_harflar);
+
+// let matn = "abc";
+// let kodlanganMatn = "";
+// for (let i = 0; i < matn.length; i++) {
+//     let keyingiHarf = String.fromCharCode(matn.charCodeAt(i) + 1);
+//     kodlanganMatn += keyingiHarf;
+// }
+
+// console.log(kodlanganMatn);
